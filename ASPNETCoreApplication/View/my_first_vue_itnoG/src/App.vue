85aa63b07c62803dd3f5b273a4d2bd78a4bc535b
HosseinMA96 <hma.globe@gmail.com>
2023-05-26 23:52:25 -0500

    Router added

--- ASPNETCoreApplication/View/my_first_vue_itnoG/src/App.vue
+++ ASPNETCoreApplication/View/my_first_vue_itnoG/src/App.vue
@@ -1,76 +1,19 @@
 <template>
-  <h2>{{ header }}</h2>
-  <p>{{ description }}</p>
-  <form v-on:submit.prevent="action">
-  <p>
-    Enter city name?
-    <input type="text" required placeholder="City name ..." v-model="cityName">
-  </p>
-  
 
-  <button type="submit">Get weather</button>
-  </form>
-
-  <br><br>
-    <table  v-show="results">
-  <thead>
-    <tr>
-      <th>temp</th>
-      <th>gust</th>
-      <th>wind</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr v-if='results'>
-      <td>{{msg.temp}}</td>
-      <td>{{msg.gust}}</td>
-      <td>{{msg.wind}}</td>
-    </tr>
-  </tbody>
-</table>
-<p v-show="blank">No such city</p>
-
+  <p>Choose what part of this page you want to see:</p>
+  <router-link to="/weather">Weather</router-link><br>
+  <router-link to="/add">Add</router-link><br>
+  <router-link to="/delete">Delete</router-link><br>
+  <router-link to="/all">All</router-link><br>
+  <router-link to="/single">single</router-link><br>
+   <div>
+   <router-view></router-view>
+   </div>
 </template>
 
-<script>
-import axios from 'axios';
-export default {
-data() {
-  return {
-      header: 'Get weather of a city',
-      description: 'Get temperature and wind of a city',
-      data: null,
-      cityName: null,
-      msg: false,
-      results: false,
-      blank: null
-  }
-},
-methods: {
-async action() {
 
-    //console.log(this.employeeId + ' ' + this.employeeName + ' ' + this.password)
-    const response = await axios.get('https://localhost:7133/api/Weather/' + this.cityName);
-    
-    if (response.data == "ERROR")
-    {
-      this.msg = "City doesn\'t exist"
-      this.blank = true
-      this.results= false
-    }
-    
-    else{
-      this.results = true
-      this.msg = {temp: response.data.temperature, gust:  response.data.gust,  wind:  response.data.wind }
-    }
-    console.log(response.data)
-
-}
-}
-};
-</script>
-
-<style scoped>
+ 
+<style>
 #red {
 font-weight: bold;
 color: rgb(144, 12, 12);
@@ -120,4 +63,23 @@ background-color: #f5f5f5;
 tr:nth-child(even) {
 background-color: #f2f2f2;
 }
+
+a {
+    display: inline-block;
+    background-color: black;
+    border: solid 1px black;
+    color: white;
+    padding: 5px;
+    margin: 10px;
+  }
+  a:hover,
+  a.router-link-active {
+    background-color: rgb(110, 79, 13);
+  }
+  div {
+    border: dashed black 1px;
+    padding: 20px;
+    margin: 10px;
+    display: inline-block;
+  }
 </style>          
--- ASPNETCoreApplication/View/my_first_vue_itnoG/src/main.js
+++ ASPNETCoreApplication/View/my_first_vue_itnoG/src/main.js
@@ -1,7 +1,29 @@
-
-
 import { createApp } from 'vue'
-import App from './App.vue'
-import axios from 'axios'
+import { createRouter, createWebHistory } from 'vue-router'
 
-createApp(App).mount('#app')
+import App from './App.vue'
+import Get_weather from './components/Get_weather.vue'
+import Delete_employee from './components/Delete_employee.vue'
+import Add_employee from './components/Add_employee.vue'
+import Show_single_employee from './components/Show_single_employee.vue'
+import Show_all_employees from './components/Show_all_employees.vue'
+
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: '/weather', component: Get_weather },
+        { path: '/delete', component: Delete_employee },
+        { path: '/add', component: Add_employee },
+        { path: '/all', component: Show_all_employees },
+        { path: '/single', component: Show_single_employee },
+    ]
+});
+
+
+
+const app = createApp(App)
+
+app.use(router);
+
+app.mount('#app')
